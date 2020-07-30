45b2d4f3aa5fdc6155943207ba2df1c3a3690f5b
Marcus <znepb@outlook.com>
2020-07-30 14:37:22 -0400

    Bump wait time to 15s

--- vendor.lua
+++ vendor.lua
@@ -2,7 +2,7 @@
 local config = require("config")
 local turtleNetworkID = config.turtleNetworkID
 
-local waitTime = 30
+local waitTime = 15
 
 local mon = peripheral.find("monitor")
 local speaker = peripheral.find("speaker")
